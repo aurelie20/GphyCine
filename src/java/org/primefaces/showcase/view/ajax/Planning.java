18ce913dc3643d434e9b8cffccb66df1e402bb05
Lilie <Lilie@Lilie-HP.home>
2015-10-10 17:25:25 +0200

    Ajout des pages pour le manager projectionniste

---
+++ src/java/org/primefaces/showcase/view/ajax/Planning.java
@@ -0,0 +1,43 @@
+package org.primefaces.showcase.view.ajax;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lilie
+ */
+public class Planning {
+    private String titre;
+    private int idSalle;
+    private String nomPerso;
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public int getIdSalle() {
+        return idSalle;
+    }
+
+    public void setIdSalle(int idSalle) {
+        this.idSalle = idSalle;
+    }
+
+    public String getNomPerso() {
+        return nomPerso;
+    }
+
+    public void setNomPerso(String nomPerso) {
+        this.nomPerso = nomPerso;
+    }
+    
+    
+}
